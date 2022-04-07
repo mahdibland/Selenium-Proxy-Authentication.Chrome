25fa2a3927b347b1cef25a1cd2fa825d75a4e741
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-04-07 15:07:35 +0430

    Cleanup

--- SeleniumProxyAuthentication.Sample/Program.cs
+++ SeleniumProxyAuthentication.Sample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
 
 namespace SeleniumProxyAuthentication.Sample
 {
--- SeleniumProxyAuthentication/ProxyAuthentication.cs
+++ SeleniumProxyAuthentication/ProxyAuthentication.cs
@@ -2,7 +2,6 @@
 using System.Runtime.InteropServices;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
 
 namespace SeleniumProxyAuthentication
 {
