0d41076ff792fe2fed03cf4cfde2fd8c8b0cef89
Mirei <42766108+mahdibland@users.noreply.github.com>
2022-04-14 12:35:35 +0430

    add important note

--- SeleniumProxyAuthentication.Sample/Program.cs
+++ SeleniumProxyAuthentication.Sample/Program.cs
@@ -1,5 +1,9 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
+// add these reference when you install the package if you clone the project there is no need you can reference to the main project
+//using SeleniumProxyAuthentication;
+//using Proxy = SeleniumProxyAuthentication.Proxy;
 
 namespace SeleniumProxyAuthentication.Sample
 {
